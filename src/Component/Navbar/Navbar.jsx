84ad3304b50cc497611960709f11c07c5ee3071e
Salah Al-Dhaferi <binaryminded@gmail.com>
2024-06-20 12:49:17 +0300

    Add navbar

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Homepage</li>
+        <li>About us</li>
+        <li>Contact us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
